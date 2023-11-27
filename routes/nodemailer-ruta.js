54bdda6bfc0f85b39015941caeab21b97d7c71cb
santiago <02monsita@gmail.com>
2023-11-27 12:07:00 -0500

    Server Gmail 1.0.0

--- routes/nodemailer-ruta.js
+++ routes/nodemailer-ruta.js
@@ -1,5 +1,5 @@
 const {Router} = require("express")
-const transporter = require("../../config/nodemailer")
+const transporter = require("/Users/Temp Tech/Desktop/ServerEmails/config/nodemailer.js")
 const nodemailer = Router()
 const cron = require('node-cron');
 
