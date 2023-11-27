074b69756be1d0b419f04790eb47cd07934bb4fc
santiago <02monsita@gmail.com>
2023-11-27 14:14:33 -0500

    Servidro de Correos 1.0.0

--- routes/nodemailer-ruta.js
+++ routes/nodemailer-ruta.js
@@ -3,29 +3,32 @@ const transporter = require("../config/nodemailer")
 const nodemailer = Router()
 const cron = require('node-cron');
 
-nodemailer.post("/", async (req, res) => {
-    const {gmail, name,mensaje,hora,programar} = req.body
-    console.log(gmail, name,mensaje,hora,programar)
-    console.log(typeof(hora))
-    if(programar === null){
-        await transporter.sendMail({
-            from:`mensaje enviado por ${name}`,
-            to: gmail,
-            text: `${mensaje}`
-        });
-        res.send('Correo enviado correctamente');
-        return
-    }
-    if (programar === 'on'){
-        const [horaCron, minutoCron] = hora.split(':');
-            cron.schedule(`${minutoCron} ${horaCron} * * *`, async () => {
-                await transporter.sendMail({
+nodemailer.post("/enviar", async (req, res) => {
+    const {gmail, name, mensaje, hora, programar} = req.body
+    try {
+        if(programar === null){
+            await transporter.sendMail({
                 from:`mensaje enviado por ${name}`,
                 to: gmail,
                 text: `${mensaje}`
-                });
-        });
-        res.send('Correo programado correctamente');
+            });
+            res.send('Correo enviado correctamente');
+            return
+        }
+        if (programar === 'on'){
+            const [horaCron, minutoCron] = hora.split(':');
+                cron.schedule(`${minutoCron} ${horaCron} * * *`, async () => {
+                    await transporter.sendMail({
+                    from:`mensaje enviado por ${name}`,
+                    to: gmail,
+                    text: `${mensaje}`
+                    });
+            });
+            res.send('Correo programado correctamente');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Hubo un error al enviar el correo');
     }
 });
 
