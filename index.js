ec735c3fe866fa676bcaae909836372f9730da1e
santiago <02monsita@gmail.com>
2023-11-27 12:29:27 -0500

    Server Gmail 1.0.0

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const { exec } = require('child_process');
 const server = new require('./server');
-exec('npm install', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error al ejecutar npm install: ${error}`);
-    return;
-  }
-
-  console.log(stdout);
-
-  server.listen(4000, () => {
-    console.log('El servidor está escuchando en el puerto 4000');
-  });
-});
-
 server.listen(4000, () => {
   console.log('Server is listening on port 4000');
 });
